b455e24d9adb9ac9fce1276be250c05c1b10c242
Xevion <xevioni@yandex.com>
2020-04-05 00:53:33 -0500

    refactor to fix misunderstood PySimpleGui specifications

--- gui.py
+++ gui.py
@@ -10,10 +10,10 @@ def main() -> None:
 
     pygui.theme('Dark Green')
     menu_column = [
-        pygui.Text('\n\n'),
-        pygui.Button('Click to Continue to Studio', key='-CONTINUE_BUTTON-', font='verdana'),
-        pygui.Button('Add Configuration', key='-CONFIG_BUTTON-', font='Verdana'),
-        pygui.Listbox(
+        [pygui.Text('\n\n')],
+        [pygui.Button('Click to Continue to Studio', key='-CONTINUE_BUTTON-', font='verdana')],
+        [pygui.Button('Add Configuration', key='-CONFIG_BUTTON-', font='Verdana')],
+        [pygui.Listbox(
             [
                 'Mechanum with Odometry',
                 'Mechanum without Odometry',
@@ -22,14 +22,18 @@ def main() -> None:
             ],
             enable_events=True, key=TitleEvents.SELECT_DRIVETRAIN, size=(25, 4),
             default_values='Mechanum with Odometry', font='verdana'
-        )
+        )]
     ]
     titleLayout = [
-        pygui.Text('Welcome to Auton Studio', text_color='Black', font='Verdana 20 bold', justification='center',
-                   size=[32, 1]),
-        pygui.Image('resources/autonStudioLogo.png'),
-        pygui.Column(menu_column)]
+        [pygui.Text('Welcome to Auton Studio', text_color='Black', font='Verdana 20 bold', justification='center',
+                   size=[32, 1])],
+        [pygui.Image('resources/autonStudioLogo.png'), pygui.Column(menu_column)],
+    ]
+
     titleWindow = pygui.Window('Auton Studio', titleLayout)
 
     while True:
         titleEvent, titleValues = titleWindow.read()
+
+if __name__ == "__main__":
+    main()
