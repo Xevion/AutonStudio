07eea58aeebbda15c1b6edd53ea7d73e787c8f57
Xevion <xevioni@yandex.com>
2020-04-07 15:55:44 -0500

    create new Helper method class

--- autonstudio/manage.py
+++ autonstudio/manage.py
@@ -4,6 +4,8 @@ Hopefully, most of the raw logic and calculations will be placed here in order t
 and allow for a overall simpler to debug and manage application.
 """
 
+import re
+
 
 class Config(object):
     """
@@ -14,3 +16,23 @@ class Config(object):
     def __init__(self):
         self.configActive = False  # Whether or not the Config Window is active.
         self.fieldConfiguration = 'None'
+
+
+class Helper(object):
+    """
+    A simple class designed to store helper methods, like number strippers or basic calculations.
+    Not to be instantiated. Static methods only.
+    """
+
+    digits = re.compile("\d+(?:\.\d+)")
+
+    @staticmethod
+    def getDigits(string):
+        """
+        Returns digits from a string. Ignores letters and spacing, regular notation numbers accepted only.
+
+        :param string: A string containing regular nontation integers and/or decimals.
+        :return: The numbers in string format in a list.
+        """
+        return Helper.digits.findAll(string)
+
